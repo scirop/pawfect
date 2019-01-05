2f7d0cd3cd5b810bed761704eb29e1b5606efbd4
Swarup Sahoo <swarupsahoo@Swarups-MacBook.local>
2019-01-04 22:22:45 -0600

    puppyrights

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, render_template
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+df = pd.read_csv('survey.csv')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+if __name__=='__main__':
+    app.run()
