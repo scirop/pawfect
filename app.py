359c7ff976a7e6fe12ea7e691fa8ab185a1c93a8
arpitmohanty9 <arpitmohanty9@gmail.com>
2019-01-25 18:01:54 -0700

    mankadami

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ app = Flask(__name__)
 
 df = pd.read_csv('survey.csv')
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/result')
 def result():
     return render_template('result.html')
+
 
 if __name__=='__main__':
     app.run()
