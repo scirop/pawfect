254539d1737378aad37e178b0cbd2c067f92485f
Arpit Mohanty <arpitmohanty9@gmail.com>
2019-02-02 01:35:48 -0700

    new clean version

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request,redirect,flash
 import pandas as pd
 import numpy as np
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
-
+CORS(app)
 df = pd.read_csv('survey.csv')
 
 
 @app.route('/')
+def base():
+    return render_template('main.html')
+
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 
 
-@app.route('/result')
-def result():
-    return render_template('result.html')
+
+# def filter_route():
+#     return render_template('filter.html')
+#
+# def filter():
+#     params = request.get_json()
+#     return jsonify(filterfunc(params, df2))
 
 
 if __name__=='__main__':
